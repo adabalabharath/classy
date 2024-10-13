6173c50af52ded7fcf1e48b6204c2cf0d46d5362
adabalabharath <saibharath.adabala@gmail.com>
2024-10-13 12:28:14 +0530

    login/signup

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -6,11 +6,24 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import axios from "axios";
 import React, { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 const Account = () => {
   const [captchaVerified, setCaptchaVerified] = useState(false);
+  const [post, setPost] = useState("");
   const [captcha, setCaptcha] = useState(false);
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [register, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   const siteKey = "6Ld2IFoqAAAAAKI0ZNbnBKLdgVPlOb6be8f-4xv5";
 
   const onCaptchaChange = (value) => {
@@ -21,6 +34,47 @@ const Account = () => {
     }
   };
 
+  const handleLogin = (e) => {
+    const { name, value } = e.target;
+    setLogin((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleRegister = (e) => {
+    const { name, value } = e.target;
+    setRegister((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleClick = async () => {
+    try {
+      if (captchaVerified && login.email && login.password) {
+        let post = await axios.post("http://localhost:7777/login", login);
+        setPost(post.data);
+        console.log(post.data);
+      } else {
+        alert("captcha is not verified");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const handleClickRegister = async () => {
+    try {
+      if (
+        captchaVerified &&
+        register.email &&
+        register.password &&
+        register.name
+      ) {
+        let post = await axios.post("http://localhost:7777/signup", register);
+        console.log(post.data);
+      } else {
+        alert("captcha is not verified");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
   return (
     <Grid container justifyContent={"center"}>
       <Grid
@@ -45,10 +99,22 @@ const Account = () => {
             Login
           </Typography>
           <Grid item>
-            <TextField label="Username or Email Address" fullWidth />
+            <TextField
+              label="Username or Email Address"
+              fullWidth
+              value={login.email}
+              name="email"
+              onChange={handleLogin}
+            />
           </Grid>
           <Grid item>
-            <TextField label="password" fullWidth />
+            <TextField
+              label="password"
+              fullWidth
+              value={login.password}
+              name="password"
+              onChange={handleLogin}
+            />
           </Grid>
           <Grid item>
             <Typography variant="subtitle2">Captcha</Typography>
@@ -60,7 +126,7 @@ const Account = () => {
             />
           </Grid>
           <Grid item>
-            <Button variant="contained" color="error">
+            <Button variant="contained" color="error" onClick={handleClick}>
               Login
             </Button>
           </Grid>
@@ -76,20 +142,43 @@ const Account = () => {
             Register
           </Typography>
           <Grid item>
-            <TextField label="Username" fullWidth />
+            <TextField
+              label="Username"
+              fullWidth
+              value={register.name}
+              name="name"
+              onChange={handleRegister}
+            />
           </Grid>
           <Grid item>
-            <TextField label="Email Address" fullWidth />
+            <TextField
+              label="Email Address"
+              fullWidth
+              value={register.email}
+              name="email"
+              onChange={handleRegister}
+            />
           </Grid>
           <Grid item>
-            <TextField label="password" fullWidth />
+            <TextField
+              label="password"
+              fullWidth
+              value={register.password}
+              name="password"
+              onChange={handleRegister}
+            />
           </Grid>
           <Grid item>
             <Typography variant="subtitle2">Captcha</Typography>
             <ReCAPTCHA sitekey={siteKey} onChange={onCaptchaChange} />
           </Grid>
           <Grid item>
-            <Button variant="contained" color="error">
+            <Button
+              variant="contained"
+              color="error"
+              disabled={post.status === "logged in successfully"}
+              onClick={handleClickRegister}
+            >
               Register
             </Button>
           </Grid>
