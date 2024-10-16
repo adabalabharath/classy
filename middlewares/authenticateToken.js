1a8be87cdcf11957e9a95288285b33ed434f7d07
adabalabharath <saibharath.adabala@gmail.com>
2024-10-16 17:15:27 +0530

    validations

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/user");
 
 const auth = async (req, res, next) => {
+
   let { token } = req.cookies;
   let verifyToken = jwt.verify(token, "classy");
   let user = await User.findById(verifyToken._id);
--- routes/bag.js
+++ routes/bag.js
@@ -1,35 +1,67 @@
 const express = require("express");
 const auth = require("../middlewares/authenticateToken");
-const bagRouter=express.Router()
+const bagRouter = express.Router();
 
-bagRouter.post('/addbag',auth,async(req,res)=>{
-   try{
-       let loggedUser=req.user
-       loggedUser.bag.push(req.body);
-       await loggedUser.save()
-       res.send({status:'added to bag',user:loggedUser})
-   }catch(error){
-       res.send({status:error.message});
-   }
-})
+bagRouter.post("/addbag", auth, async (req, res) => {
+  try {
+    let loggedUser = req.user;
+    loggedUser.bag.push(req.body);
+    await loggedUser.save();
+    res.send({ status: "added to bag", user: loggedUser });
+  } catch (error) {
+    res.send({ status: error.message });
+  }
+});
 
-bagRouter.delete('/remove/:case',auth,async(req,res)=>{
-    try{
-       let name=req.params.case
-       let user=req.user
-       let caseValidation=user.bag.filter(x=>x.name===name)
-       if(caseValidation.length===0){
-        throw new Error('No product found')
-       }
-       let filtered=user.bag.filter(x=>x.name!==name)
-       
-       user.bag=filtered
-       await user.save()
-       res.send({status:'removed item successfully',user})
-    }catch(error){
-      res.send({status:error.message});
+bagRouter.patch("/:call/:case/:count?", auth, async (req, res) => {
+  try {
+    const name = req.params.case;
+    console.log(name)
+    const user = req.user;
+    const count = req.params.count 
+    const validCalls = ['remove', 'count'];
+
+    // Validate the "call" parameter
+    if (!validCalls.includes(req.params.call)) {
+      throw new Error('Not a valid call');
     }
-})
+
+    // Find the product in the user's bag
+    const caseValidation = user.bag.find((x) => x.name === name);
+    if (!caseValidation) {
+      throw new Error("No product found");
+    }
+
+    // If the call is "remove", remove the item from the bag
+    if (req.params.call === 'remove' && !count) {
+      user.bag = user.bag.filter((x) => x.name !== name);
+    } else if (req.params.call === 'count') {
+      // If the call is "count", update the item's count
+      if (!count) {
+        throw new Error("Invalid count provided");
+      }
+      
+      user.bag = user.bag.map((item) => {
+        if (item.name === name) {
+          item.count=count; // Update the count for the specific item
+          return item
+        }
+        return item // Return other items unchanged
+      });
+    }
+
+    if (req.params.call === 'remove' && count) {
+         throw new Error('check url again')
+    }
+
+    // Save the updated user object
+    await user.save();
+
+    res.send({ status: "Updated item successfully", user });
+  } catch (error) {
+    res.status(400).send({ status: error.message });
+  }
+});
 
 
-module.exports=bagRouter
+module.exports = bagRouter;
